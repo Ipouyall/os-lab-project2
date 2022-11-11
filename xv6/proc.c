0057733e5a0109ad55ae67191665063064f272b6
pouya <pouyasadeghi2012@gmail.com>
2022-11-11 21:27:50 +0330

    Syscall/find largest prime factor (#3)
    * modify syscall.h
    
    * modify syscall.c
    
    * modify syscall.c
    
    * modify usys.S
    
    * modify user.h
    
    * fixed sysproc.c
    
    * add implementation to proc.c
    
    * add test_bpf
    
    * fix proc.c/bpf
    
    * update xv6/Makefile
    
    * fix sysproc.c/sys_bpf
    
    * fix test_bpf
    
    * fixed args passing bug
    
    * add error handling
    
    * use complete name instead of short form
    
    * refactor comments

--- xv6/proc.c
+++ xv6/proc.c
@@ -532,3 +532,35 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+// find the largest prime factor of a number
+int
+find_largest_prime_factor(int n)
+{
+    int maxPrime = -1;
+
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n = n / 2;
+    }
+    while (n % 3 == 0) {
+        maxPrime = 3;
+        n = n / 3;
+    }
+
+    for (int i = 5; i <= n; i += 6) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+        while (n % (i + 2) == 0) {
+            maxPrime = i + 2;
+            n = n / (i + 2);
+        }
+    }
+
+    if (n > 4)
+        maxPrime = n;
+
+    return maxPrime;
+}
